04618ea23954a91ffc4f5f87631211b2da0c94bb
Nora Alsultan. <97395867+Norakhalsu@users.noreply.github.com>
2024-08-07 00:50:44 +0300

    Update UserService.java

--- src/main/java/com/example/capstone1_amazoneclone/Service/UserService.java
+++ src/main/java/com/example/capstone1_amazoneclone/Service/UserService.java
@@ -57,65 +57,7 @@ public class UserService {
     }
 
 
-                                      // ---------- Extra Point ----------
-
-    // Costumer Have Gift when buy with determine price
-    public String giftCard(String userId, double productPrice) {
-
-        User user = getUserById(userId);
-
-        if (user == null) {
-            return "User not found";
-        }
-
-        double totalSpent = user.getBalance() - productPrice;
-
-        if (totalSpent >= 1000) {
-            return "Congratulations, you have a gift card with 600 Riyal!";
-        } else if (totalSpent >= 800) {
-            return "Congratulations, you have a gift card with 500 Riyal!";
-        } else if (totalSpent >= 500) {
-            return "Congratulations, you have a gift card with 200 Riyal!";
-        } else {
-            return "You have not spent enough to qualify for a gift card.";
-        }
-    }
-
-
-                                            // ------ extra credit -------
-
-    // user log in with phone number and get a verification Code
-    public String login(String phoneNumber) {
-
-        for (User user : users) {
-            if (user.getPhoneNumber().equals(phoneNumber)) {
-                int verificationCode = (int) (Math.random() * 9000) + 1000; // Generate a 4-digit random verification code
-                // Send verification code logic here
-                return "Verification code: " + verificationCode;
-            }
-        }
-        return "Phone number is not correct";
-    }
-
-
-
-//    public ArrayList<Orders> getOrders() {
-//        return getOrders();
-//    }
-
-
-
-    // Costumer Have Problem and need to connect with support team
-    public String connectToSupportCenter(String Name, String issueDescription) {
-
-        String phoneNumberSupport = "0509875764";
-
-        if (Name != null && !Name.isEmpty()) {
-            return "Please contact our support team at phone number: " + phoneNumberSupport;
-        }
-        return "Error: User name is missing. Please provide a valid user name.";
-    }
-
+      
 
     // get user by id
     public User getUserById(String id) {
